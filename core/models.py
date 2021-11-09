8c18627c243505e2efd5b7edb6f46eea4dc2ad58
Souza <tec.psouza@gmail.com>
2021-11-09 03:48:54 -0300

    criado tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -8,6 +8,7 @@ class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     derscricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    local = models.CharField(max_length=100, blank=True, null=True)
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -16,3 +17,6 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from core.models import Evento
 
 # Create your views here.
+
+
+# def index(request):
+#     return redirect('/agenda/') # from django.shortcuts import redirect
+
+def lista_eventos(request):
+    #  usuario = request.user
+    #  evento = Evento.objects.filter(usuario=usuario)
+    #  evento = Evento.objects.get(id=1) # busca um registro específico
+    evento = Evento.objects.all()  # CUIDADO com o tamanho do banco de dados ao ser utilizado "ALL"
+    dados = {'eventos': evento}  # cria um dicionário
+    return render(request, 'agenda.html', dados)
